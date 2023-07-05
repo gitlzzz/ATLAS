bcfa4d0be2cc3b4f8f0f41d1f1e08d1973e0fee7
Pol Sanz <psanz@iciq.es>
2023-07-05 17:02:03 +0200

    Added PhaseNotFound exception

--- src/MatDBForge/core/exceptions.py
+++ src/MatDBForge/core/exceptions.py
@@ -1,2 +1,15 @@
 class BaseStructureNotFound(Exception):
     pass
+
+
+class PhaseNotFound(Exception):
+    def __init__(self, phase_diagram, given_phase):
+        self.phases = phase_diagram.phase_names
+        self.given_phase = given_phase
+
+    def __str__(self):
+
+        return (
+            f"The given phase '{self.given_phase}' str is not found in the phase diagram, "
+            f"which has the following phases:\n {self.phases}"
+        )
