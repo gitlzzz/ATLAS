2b07f65741f4ae6c591e77055dc317d8c307a6b4
Pol Sanz <psanz@iciq.es>
2023-07-05 17:02:54 +0200

    Added more verbose printing to initial database example

--- src/MatDBForge/examples/create_init_db_new.py
+++ src/MatDBForge/examples/create_init_db_new.py
@@ -17,18 +17,18 @@ PHASES = [
 # to be generated from each base structure
 #
 # Number of replacement percentages to be computed per base structure
-NUM_STRUCT = 50
+NUM_STRUCT = 15
 
 # Number of random replacements to be done for a single percentage
-NUM_REPEAT = 5
+NUM_REPEAT = 10
 
 # Where the optimized base structures are located
 RELAX_STRUCT_PATH = "/home/psanz/teklahome/projects/p2-CuZn/relaxed_structures_initialdb/relaxed_structures"
 
 # Where to store the initial database once ready
 SAVE_PATH = (
-    "/home/psanz/teklahome/projects/p2-CuZn/relaxed_structures_initialdb/initial_db"
-    # "/tmp"
+    # "/home/psanz/teklahome/projects/p2-CuZn/relaxed_structures_initialdb/initial_db"
+    "/tmp"
 )
 
 current_time = time.strftime("%d%m%Y-%H%M%S")
@@ -42,6 +42,7 @@ structures = indb.CuZnInitialDatabase(
 structures.read_base_structures(path=RELAX_STRUCT_PATH, target_structures=PHASES)
 
 # Generating perturbed structures for every base structure.
+# for phase in PHASES:
 for phase in PHASES:
     # Getting properties for the current phase
     phase = structures.CUZN_PHASES.get_phase(phase)
@@ -59,14 +60,14 @@ for phase in PHASES:
 
 print()
 ut.custom_print(f"Done! {len(structures.df.index)} structures generated.", "done")
-
 print()
 
 # Checking for duplicate structures, and deleting the ones that are repeated.
 structures.find_repeat_structures(delete=True)
 ut.custom_print(structures, "info")
-print()
+# print()
 
+ut.custom_print("Adding structures displaced around the minimum...", "info")
 # Displacing structures around PES minima by modifying the
 # relaxed cell lattice parameters.
 structures.perturb_min_displacement(frac_max=0.05, repeat=40)
@@ -75,14 +76,14 @@ ut.custom_print("Displacements around minimum done.", "done")
 ut.custom_print(structures, "info")
 print()
 
+ut.custom_print("Adding a random perturbation to structures...", "info")
 # Adding a random perturbation to structures.
 structures.perturb_gauss(center=0.04, repeat=7)
-
 ut.custom_print("Random perturbation done.", "done")
 ut.custom_print(structures, "info")
 print()
 
 # Saving database
-ut.custom_print("Checking for incorrect phase assignation", "info")
+ut.custom_print("Checking for incorrect phase assignation...", "info")
 indb.check_incorrect_ratios(structures.df, indb.CuZnInitialDatabase.CUZN_PHASES)
-structures.save_database(path=SAVE_PATH, suffix="final")
+structures.save_database(path=SAVE_PATH, suffix="test_new_db_style")
