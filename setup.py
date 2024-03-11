04110494e12bd2a842abf9edc7fc319726c48920
Pol Sanz <psanz@iciq.es>
2024-03-11 09:43:41 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="MatDBForge",
-    version="0.2.1",
+    version="0.2.4",
     description="MatDBForge",
     author="Pol Sanz",
     author_email="me@polsanz.xyz",
