036379dad1aedcb158d6258a6a5f456cf0db7708
Pol Sanz <psanz@iciq.es>
2024-12-28 17:00:36 +0100

    chore(core): add versioning information to sphinx documentation

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,5 @@
 """Sphinx configuration file."""
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -14,17 +15,17 @@
 import pathlib as pl
 import sys
 
-sys.path.insert(0, pl.Path('../src/MatDBForge').absolute())
+sys.path.insert(0, pl.Path("../src/MatDBForge").absolute())
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'MatDBForge'
-copyright = '2024, Pol Sanz'
-author = 'Pol Sanz'
+project = "MatDBForge"
+copyright = "2024, Pol Sanz"
+author = "Pol Sanz"
 
 # The full version, including alpha/beta/rc tags
-release = '0.19.10'
+release = "0.19.10"
 
 
 # -- General configuration ---------------------------------------------------
@@ -33,28 +34,29 @@ release = '0.19.10'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.napoleon',
-    'myst_parser',
-    'sphinx.ext.autodoc',
+    "sphinx.ext.napoleon",
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx_multiversion",
     #    'sphinx.ext.viewcode',
     #    'sphinx_autodoc_typehints'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = [
-    '_build',
-    'Thumbs.db',
-    '.DS_Store',
-    'benchmarks/*',
-    'tests/*',
-    'devel/*',
-    'display_db/*',
-    '*sync-conflict*',
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    "benchmarks/*",
+    "tests/*",
+    "devel/*",
+    "display_db/*",
+    "*sync-conflict*",
 ]
 
 
@@ -63,32 +65,33 @@ exclude_patterns = [
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 html_css_files = [
-    'css/custom.css',
+    "css/custom.css",
 ]
 
-myst_enable_extensions = ['colon_fence', 'html_image', 'dollarmath']
+myst_enable_extensions = ["colon_fence", "html_image", "dollarmath"]
 
 
-html_favicon = './source/favicon.svg'
-html_logo = '../media/logo_dark.png'
+html_favicon = "./source/favicon.svg"
+html_logo = "../media/logo_dark.png"
 
 html_theme_options = {
-    'logo_only': True,
-    'version_selector': True,
+    "logo_only": True,
+    "version_selector": True,
 }
 
 html_context = {
-    'display_github': True,  # Integrate GitHub
-    'github_user': 'pol-sb',  # Username
-    'github_repo': 'MatDBForge',  # Repo name
-    'github_version': 'master',  # Version
-    'conf_py_path': '/docs/',  # Path in the checkout to the docs root
+    "display_github": True,  # Integrate GitHub
+    "github_user": "pol-sb",  # Username
+    "github_repo": "MatDBForge",  # Repo name
+    "github_version": "master",  # Version
+    "conf_py_path": "/docs/",  # Path in the checkout to the docs root
+    "display_lower_left": True,
 }
