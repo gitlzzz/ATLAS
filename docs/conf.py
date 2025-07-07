837d2e341e2975a7a3691030cf07381e5639d043
Pol Sanz <psanz@iciq.es>
2025-07-07 10:26:11 +0200

    fix(core): update manually version

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ copyright = "2024, Pol Sanz"
 author = "Pol Sanz"
 
 # The full version, including alpha/beta/rc tags
-release = "0.30.2"
+release = "0.31.1"
 
 
 # -- General configuration ---------------------------------------------------
