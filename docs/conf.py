024a5783ca9a4aac1facbcb9b6e0ab4c302d0106
Pol Sanz <psanz@iciq.es>
2026-02-04 14:25:07 +0100

    bump: version 0.48.2 → 0.48.3

--- docs/conf.py
+++ docs/conf.py
@@ -84,7 +84,7 @@ copyright = "2024, Pol Sanz"
 author = "Pol Sanz"
 
 # The full version, including alpha/beta/rc tags
-release = "0.48.2"
+release = "0.48.3"
 
 
 # -- General configuration ---------------------------------------------------
